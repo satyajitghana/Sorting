cd3f646707be2f55ebbe8d62b94c2270768c0785
Satyajit Ghana <satyajitghana7@gmail.com>
2016-12-13 19:40:35 +0530

    Initial Commit

---
+++ Sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorting
+//
+//  Created by Satyajit Ghana on 13/12/16.
+//  Copyright © 2016 Satyajit Ghana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
